efcf8f3851023cc5e48c420e945b0ae733bbbe7d
DoctorTone <tony@drt-software.com>
2024-01-06 19:48:50 +0000

    Make buying choices persistent

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,43 +1,94 @@
+import { useRef } from "react";
 import { useStore } from "../state/store";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { FormEvent } from "react";
 
 const ShoppingList = () => {
+  const itemRef = useRef<HTMLInputElement | null>(null);
   const shoppingItems = useStore((state) => state.shoppingItems);
-  const sortedList = shoppingItems.map((item) => {
-    const lower = item.toLowerCase();
+  // DEBUG
+  console.log("Items = ", shoppingItems);
+  const addItem = useStore((state) => state.addItem);
+  const updateItem = useStore((state) => state.updateItem);
+  const sortedList = shoppingItems.map((shoppingItem) => {
+    const lower = shoppingItem.item.toLowerCase();
     const capital = lower.charAt(0).toUpperCase() + lower.slice(1);
-    return capital;
+    return {
+      item: capital,
+      required: shoppingItem.required,
+    };
   });
-  sortedList.sort();
 
-  const shoppingListItems = sortedList.map((item) => (
-    <li key={item} className="h4">
-      <Form.Check type={"checkbox"} label={item} />
-    </li>
+  sortedList.sort((a, b) => {
+    const nameA = a.item.toLowerCase();
+    const nameB = b.item.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  // Ensure no duplicates
+  const singleList = sortedList.filter(
+    (elem, index) => sortedList.indexOf(elem) === index
+  );
+
+  const tickItem = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // DEBUG
+    console.log("Ticked it", event.target.checked);
+    updateItem(event.target.name, event.target.checked);
+  };
+
+  const shoppingListItems = singleList.map((elem) => (
+    <>
+      <Col xs={9}>
+        <div key={elem.item} className="h4">
+          <Form.Check
+            type={"checkbox"}
+            label={elem.item}
+            name={elem.item}
+            onChange={tickItem}
+            checked={elem.required}
+          />
+        </div>
+      </Col>
+      <Col xs={2}>
+        <input className="w-100" type="number" placeholder="1" />
+      </Col>
+      <Col xs={1}></Col>
+    </>
   ));
 
-  const addItem = () => {};
+  const updateList = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log("Item = ", itemRef.current?.value);
+    addItem(itemRef.current!.value);
+  };
 
   return (
     <>
-      <Form onSubmit={addItem} className="mb-3">
+      <Form onSubmit={updateList} className="mb-3">
         <Row>
-          <Col xs={6}>
-            <Form.Control name="item" type="text" />
+          <Col xs={9}>
+            <Form.Control ref={itemRef} name="item" type="text" />
           </Col>
-          <Col xs={4}>
+          <Col xs={3}>
             <Button type="submit" variant="primary">
               Add
             </Button>
           </Col>
         </Row>
       </Form>
-      <Row>
-        <ul>{shoppingListItems}</ul>
-      </Row>
+      <Row>{shoppingListItems}</Row>
     </>
   );
 };
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,98 +1,126 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-type ShoppingStoreState = {
-  shoppingItems: string[];
+type ShoppingItem = {
+  item: string;
+  required: boolean;
 };
 
-export const useStore = create<ShoppingStoreState>()(() => ({
-  shoppingItems: [
-    "bread",
-    "milk",
-    "beer",
-    "wine",
-    "chips",
-    "cobs",
-    "tomatoes",
-    "pizza",
-    "garlic bread",
-    "fish",
-    "pasta",
-    "pasta sauce",
-    "curry sauce",
-    "rice",
-    "tea",
-    "coffee",
-    "pepsi",
-    "chilli",
-    "guacomole",
-    "sour cream",
-    "fajitas",
-    "tortilla",
-    "baguette",
-    "cake",
-    "potatoes",
-    "chicken",
-    "pie",
-    "birthday card",
-    "pork pie",
-    "luncheon meat",
-    "corned beef",
-    "ham",
-    "baked beans",
-    "kidney beans",
-    "bananas",
-    "carrots",
-    "peas",
-    "biscuits",
-    "cucumber",
-    "lettuce",
-    "mushrooms",
-    "onion",
-    "water",
-    "crisps",
-    "pepper",
-    "salad",
-    "cheese",
-    "cream",
-    "trifles",
-    "yoghurt",
-    "cheesecake",
-    "eggs",
-    "margarine",
-    "soya milk",
-    "bacon",
-    "duck",
-    "mashed potato",
-    "mince",
-    "sausages",
-    "tuna",
-    "vinegar",
-    "salt",
-    "gravy",
-    "tomato sauce",
-    "brown sauce",
-    "salad cream",
-    "mayonnaise",
-    "salt",
-    "fish fingers",
-    "soup",
-    "soap",
-    "wash powder",
-    "washing up liquid",
-    "fabric conditioner",
-    "yorkshire pudding",
-    "notebook",
-    "porridge",
-    "spaghetti",
-    "chocolate",
-    "cider",
-    "lemonade",
-    "foil",
-    "bathroom cleaner",
-    "calgon",
-    "kitchen cleaner",
-    "kitchen roll",
-    "pasty",
-    "scouring pads",
-  ],
-}));
+type ShoppingStoreState = {
+  shoppingItems: ShoppingItem[];
+  addItem: (item: string) => void;
+  updateItem: (item: string, buyit: boolean) => void;
+};
+
+export const useStore = create<ShoppingStoreState>()(
+  devtools(
+    persist(
+      (set) => ({
+        shoppingItems: [
+          { item: "bread", required: false },
+          { item: "milk", required: false },
+          { item: "beer", required: false },
+          { item: "wine", required: false },
+          { item: "chips", required: false },
+          { item: "cobs", required: false },
+          { item: "tomatoes", required: false },
+          { item: "pizza", required: false },
+          { item: "garlic bread", required: false },
+          { item: "fish", required: false },
+          { item: "pasta", required: false },
+          { item: "pasta sauce", required: false },
+          { item: "curry sauce", required: false },
+          { item: "rice", required: false },
+          { item: "tea", required: false },
+          { item: "coffee", required: false },
+          { item: "pepsi", required: false },
+          { item: "chilli", required: false },
+          { item: "guacomole", required: false },
+          { item: "sour cream", required: false },
+          { item: "fajitas", required: false },
+          { item: "tortilla", required: false },
+          { item: "baguette", required: false },
+          { item: "cake", required: false },
+          { item: "potatoes", required: false },
+          { item: "chicken", required: false },
+          { item: "pie", required: false },
+          { item: "birthday card", required: false },
+          { item: "pork pie", required: false },
+          { item: "luncheon meat", required: false },
+          { item: "corned beef", required: false },
+          { item: "ham", required: false },
+          { item: "baked beans", required: false },
+          { item: "kidney beans", required: false },
+          { item: "bananas", required: false },
+          { item: "carrots", required: false },
+          { item: "peas", required: false },
+          { item: "biscuits", required: false },
+          { item: "cucumber", required: false },
+          { item: "lettuce", required: false },
+          { item: "mushrooms", required: false },
+          { item: "onion", required: false },
+          { item: "water", required: false },
+          { item: "crisps", required: false },
+          { item: "pepper", required: false },
+          { item: "salad", required: false },
+          { item: "cheese", required: false },
+          { item: "cream", required: false },
+          { item: "trifles", required: false },
+          { item: "yoghurt", required: false },
+          { item: "cheesecake", required: false },
+          { item: "eggs", required: false },
+          { item: "margarine", required: false },
+          { item: "soya milk", required: false },
+          { item: "bacon", required: false },
+          { item: "duck", required: false },
+          { item: "mashed potato", required: false },
+          { item: "mince", required: false },
+          { item: "sausages", required: false },
+          { item: "tuna", required: false },
+          { item: "vinegar", required: false },
+          { item: "salt", required: false },
+          { item: "fish fingers", required: false },
+          { item: "soup", required: false },
+          { item: "soap", required: false },
+          { item: "wash powder", required: false },
+          { item: "washing up liquid", required: false },
+          { item: "fabric conditioner", required: false },
+          { item: "yorkshire pudding", required: false },
+          { item: "notebook", required: false },
+          { item: "porridge", required: false },
+          { item: "spaghetti", required: false },
+          { item: "chocolate", required: false },
+          { item: "cider", required: false },
+          { item: "lemonade", required: false },
+          { item: "foil", required: false },
+          { item: "bathroom cleaner", required: false },
+          { item: "calgon", required: false },
+        ],
+        addItem: (name: string) =>
+          set((state) => ({
+            shoppingItems: [
+              ...state.shoppingItems,
+              { item: name, required: false },
+            ],
+          })),
+        updateItem: (name: string, buyit: boolean) => {
+          const lowerName = name.toLocaleLowerCase();
+          set((state) => ({
+            shoppingItems: state.shoppingItems.map((elem) => {
+              if (elem.item === lowerName) {
+                const newItem = {
+                  item: lowerName,
+                  required: buyit,
+                };
+                return newItem;
+              }
+
+              return elem;
+            }),
+          }));
+        },
+      }),
+      { name: "shopStore_v0.1" }
+    )
+  )
+);
